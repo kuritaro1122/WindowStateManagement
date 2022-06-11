5b2e51f79819a130c22275dd6d3cb840b94c7091
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2022-06-11 18:41:22 +0900

    Add files via upload

--- WindowControl.cs
+++ WindowControl.cs
@@ -1,14 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using WindowStateManagement;
+//#if UNITY_EDITOR
+using UnityEditor;
+//#endif
 
 namespace WindowStateManagement {
     public class WindowControl : MonoBehaviour {
         [SerializeField] WindowStateManager manager = null;
         [SerializeField] public GameObject firstSelectUI = null;
+        [SerializeField] public bool InitOnStart = true;
+        [SerializeField] Selectable[] selectables = null;
+        [SerializeField] bool[] selectables_interactable = null;
+        public bool Interactable { get; private set; } = true;
+
         public void ChangeMainWindow() => manager.ChangeMainWindow();
         public void ChangeWindow(int index) => manager.ChangeWindow(index);
         public void ChangeThisWindow() => manager.ChangeThisWindow(this);
         public int GetThisWindowIndex() => manager.GetThisWindowIndex(this);
+
+        void Start() {
+            if (this.InitOnStart) InitSelectables();
+        }
+
+        public void InitSelectables() {
+            this.selectables = this.GetComponentsInChildren<Selectable>();
+            this.selectables_interactable = new bool[this.selectables.Length];
+            for (int i = 0; i < this.selectables.Length; i++) {
+                this.selectables_interactable[i] = this.selectables[i].interactable;
+            }
+        }
+        public void SetInteractable(bool interactable) {
+            this.Interactable = interactable;
+            if (this.selectables == null || this.selectables.Length != this.selectables_interactable.Length) {
+#if UNITY_EDITOR
+                Debug.LogWarning("WindowsControl/Prease init!! (editor only)");
+#endif
+                InitSelectables();
+            }
+            for (int i = 0; i < this.selectables.Length; i++) {
+                if (this.selectables_interactable[i]) {
+                    this.selectables[i].interactable = this.Interactable;
+                }
+            }
+        }
+
+        public void SelectWindow(EventSystem eventSystem) {
+            if (eventSystem == null) {
+                Debug.LogWarning("WindowStateManager/EventSystem is null.");
+                return;
+            }
+            eventSystem.firstSelectedGameObject = this.firstSelectUI;
+            if (this.firstSelectUI != null) {
+                eventSystem.SetSelectedGameObject(this.firstSelectUI);
+            }
+        }
+        public void SelectWindow() => SelectWindow(EventSystem.current);
     }
 }
+
--- WindowStateManager.cs
+++ WindowStateManager.cs
@@ -2,41 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using WindowStateManagement;
+//#if UNITY_EDITOR
 using UnityEditor;
+//#endif
 
 namespace WindowStateManagement {
     [AddComponentMenu("WindowStateManagement/WindowStateManager")]
     public class WindowStateManager : MonoBehaviour {
         public const int MainWindowIndex = -1;
         [SerializeField, Min(-1)] int index = MainWindowIndex;
-        public int Index => this.index;
+        public int CurrentIndex => this.index;
         [SerializeField] WindowControl mainWindow = null;
-        [SerializeField] WindowControl[] windows = null;
+        [SerializeField] List<WindowControl> windows = new List<WindowControl>();
+        private List<WindowControl> popupWindows = new List<WindowControl>();
+        public bool ExistPopupWindows => this.popupWindows.Count > 0;
+        //private bool popup = false;
 
-        public void ChangeMainWindow() => this.ChangeWindow(MainWindowIndex);
-        public void ChangeWindow(int index) {
-            
-            this.index = index;
-            for (int i = 0; i < this.windows.Length; i++) {
-                WindowControl w = this.windows[i];
-                ChangeWindow(w, this.index == i);
+        void Update() {
+            UpdatePopupWindow();
+        }
+
+        public WindowControl CurrentWindow {
+            get {
+                if (this.index == WindowStateManager.MainWindowIndex) return this.mainWindow;
+                else return this.windows[this.index];
             }
-            if (this.mainWindow != null) ChangeWindow(this.mainWindow, this.index == MainWindowIndex);
-            void ChangeWindow(WindowControl window, bool change) {
-                window.gameObject.SetActive(change);
-                if (change) SetWindowToEventSystem(window);
-            }
-            void SetWindowToEventSystem(WindowControl window) {
-                EventSystem eventSystem = EventSystem.current;
-#if UNITY_EDITOR
-                if (eventSystem == null) {
-                    Debug.Log("WindowStateManager/EventSystem is null.");
-                    return;
+        }
+
+        private void UpdatePopupWindow() {
+            if (!this.ExistPopupWindows) return;
+            if (this.popupWindows[0] == null || !this.popupWindows[0].gameObject.activeInHierarchy) {
+                this.popupWindows.RemoveAt(0);
+                if (this.ExistPopupWindows) {
+                    this.CurrentWindow.SetInteractable(false);
+                    WindowControl popupWindow = this.popupWindows[0];
+                    popupWindow.gameObject.SetActive(true);
+                    popupWindow.SetInteractable(true);
+                    popupWindow.SelectWindow();
+                } else {
+                    this.CurrentWindow.gameObject.SetActive(true);
+                    this.CurrentWindow.SetInteractable(true);
+                    this.CurrentWindow.SelectWindow();
                 }
-#endif
-                eventSystem.firstSelectedGameObject = window.gameObject;
-                if (window.firstSelectUI != null)
-                    eventSystem.SetSelectedGameObject(window.firstSelectUI);
+            }
+        }
+
+        public void ShowPopupWindows(params WindowControl[] popupWindows) {
+            foreach (var w in popupWindows) ShowPopupWindow(w);
+        }
+        public void ShowPopupWindow(WindowControl popupWindow) {
+            if (popupWindow == null) {
+                Debug.LogError($"WindowStateManager/ShowPopupWindow/this window is null.");
+                return;
+            }
+            if (this.ExistPopupWindows) {
+                popupWindow.gameObject.SetActive(false);
+            } else {
+                this.CurrentWindow.SetInteractable(false);
+                popupWindow.gameObject.SetActive(true);
+                popupWindow.SetInteractable(true);
+                popupWindow.SelectWindow();
+            }
+            this.popupWindows.Add(popupWindow);
+        }
+
+        public void ChangeMainWindow() => this.ChangeWindow(WindowStateManager.MainWindowIndex);
+        public void ChangeWindow(int index) {
+            this.index = index;
+            for (int i = 0; i < this.windows.Count; i++) {
+                WindowControl w = this.windows[i];
+                if (i == this.index) {
+                    w.gameObject.SetActive(true);
+                    w.SetInteractable(true);
+                    w.SelectWindow();
+                } else {
+                    w.gameObject.SetActive(false);
+                }
+            }
+            if (this.mainWindow != null) {
+                if (this.index == WindowStateManager.MainWindowIndex) {
+                    this.mainWindow.gameObject.SetActive(true);
+                    this.mainWindow.SetInteractable(true);
+                    this.mainWindow.SelectWindow();
+                } else {
+                    this.mainWindow.gameObject.SetActive(false);
+                }
             }
         }
         public void ChangeThisWindow(WindowControl window) {
@@ -44,28 +95,58 @@ namespace WindowStateManagement {
             ChangeWindow(index);
         }
         public int GetThisWindowIndex(WindowControl window) {
-            for (int i = 0; i < this.windows.Length; i++) {
+            for (int i = 0; i < this.windows.Count; i++) {
                 if (this.windows[i] == window) return i;
             }
-            Debug.LogError($"WindowStateManager/this window was not attacked. window name:{window.gameObject.name}");
+            if (window == null || window.gameObject == null) {
+                Debug.LogError($"WindowStateManager/this window is null.");
+            } else {
+#if UNITY_EDITOR
+                Debug.Log($"WindowStateManager/this window was not attacked. window name:{window.gameObject.name}");
+#endif
+                this.windows.Add(window);
+                return GetThisWindowIndex(window);
+            }
             return -2;
         }
     }
+}
+
 #if UNITY_EDITOR
-    [CustomEditor(typeof(WindowStateManager))]
-    internal class WindowStateManagerEditor : Editor {
-        private bool attachWindow = true;
-        private int index = WindowStateManager.MainWindowIndex;
-        private WindowControl window;
-        public override void OnInspectorGUI() {
-            base.OnInspectorGUI();
-            var cls = target as WindowStateManager;
-            EditorGUILayout.LabelField("~~~ Controller ~~~");
-            this.attachWindow = EditorGUILayout.ToggleLeft("Attach Window", this.attachWindow);
-            if (this.attachWindow) this.window = EditorGUILayout.ObjectField("Window", this.window, typeof(WindowControl), true) as WindowControl;
-            else this.index = EditorGUILayout.IntField("Index", this.index);
-            if (GUILayout.Button("ChangeWindow")) cls.ChangeWindow(this.index);
+[CustomEditor(typeof(WindowStateManager))]
+internal class WindowStateManagerEditor : Editor {
+    //changeWindow
+    private bool attachWindow = false;
+    private int index = WindowStateManager.MainWindowIndex;
+    private WindowControl window;
+    //popupWindow
+    [SerializeField] List<WindowControl> popupWindows = new List<WindowControl>();
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        var cls = target as WindowStateManager;
+        EditorGUILayout.LabelField("~~~ Controller ~~~");
+        EditorGUILayout.Space(4f);
+        EditorGUILayout.LabelField("--- Change Window ---");
+        this.attachWindow = EditorGUILayout.ToggleLeft("Attach Window", this.attachWindow);
+        if (this.attachWindow) this.window = EditorGUILayout.ObjectField("Window", this.window, typeof(WindowControl), true) as WindowControl;
+        else this.index = EditorGUILayout.IntField("Index", this.index);
+        if (GUILayout.Button("ChangeWindow")) {
+            if (this.attachWindow) cls.ChangeThisWindow(this.window);
+            else cls.ChangeWindow(this.index);
+        }
+        EditorGUILayout.Space(4f);
+        EditorGUILayout.LabelField("--- Popup Window ---");
+        var so = new SerializedObject(this);
+        so.Update();
+        EditorGUILayout.PropertyField(so.FindProperty("popupWindows"), true);
+        so.ApplyModifiedProperties();
+        if (Application.isPlaying) {
+            if (GUILayout.Button("Show Popup Window")) {
+                cls.ShowPopupWindows(this.popupWindows.ToArray());
+            }
+        } else {
+            EditorGUILayout.HelpBox("Execute is playing time only.", MessageType.Info);
         }
     }
-#endif
 }
+#endif
